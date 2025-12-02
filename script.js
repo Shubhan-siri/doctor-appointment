8031fa1d9939f0d517f08ef174a65b4bbd95c373
Shubhan-siri <shubhanshubusines786@gmail.com>
2025-12-02 17:48:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+// SIGN UP
 function signup() {
   const name = document.getElementById("signupName").value;
   const email = document.getElementById("signupEmail").value;
   const pass = document.getElementById("signupPassword").value;
 
-  if (!name || !email || !pass)
-    return alert("Please fill all fields!");
+  if (!name || !email || !pass) {
+    alert("Please fill all fields!");
+    return;
+  }
 
+  // Save user data
   localStorage.setItem("user", JSON.stringify({ name, email, pass }));
 
-  alert("Signup successful! Now Sign In.");
+  alert("Signup successful! Now please Sign In.");
 }
 
+
+// LOGIN
 function login() {
   const email = document.getElementById("loginEmail").value;
   const pass = document.getElementById("loginPassword").value;
 
   const user = JSON.parse(localStorage.getItem("user"));
 
-  if (!user)
-    return alert("No account found. Please Sign Up first!");
+  if (!user) {
+    alert("No account found. Please Sign Up first!");
+    return;
+  }
 
   if (email === user.email && pass === user.pass) {
     alert("Login successful!");
-    window.location.href = "home.html"; // Next page
+    window.location.href = "home.html";
   } else {
     alert("Incorrect Email or Password");
   }
